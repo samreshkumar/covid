24e2568bd8663327cef1f91c8fcb569c8caa75a5
samresh kumar <samresh.kumar@armworldwide.com>
2020-10-20 17:03:04 +0530

    Updates

---
+++ precache-manifest.c943a8d45cf216f785f85db1e1739ada.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2683346721ed4baddaf8a7d47e72b173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eabd09a18d0afed10297",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "9a2c4a6e12a50c081a1e",
+    "url": "/static/css/main.fb1fc683.chunk.css"
+  },
+  {
+    "revision": "eabd09a18d0afed10297",
+    "url": "/static/js/2.666e7c82.chunk.js"
+  },
+  {
+    "revision": "7f01fb7a78c6328cf95a6a5991494193",
+    "url": "/static/js/2.666e7c82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a2c4a6e12a50c081a1e",
+    "url": "/static/js/main.2d62950c.chunk.js"
+  },
+  {
+    "revision": "4df097abbea25f86dac0",
+    "url": "/static/js/runtime-main.f10a8730.js"
+  }
+]);
